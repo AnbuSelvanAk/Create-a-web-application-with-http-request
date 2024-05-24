244680b1fd0ae94f439d1e0c1fb9df29f7d72d81
AnbuSelvanAk <166841969+AnbuSelvanAk@users.noreply.github.com>
2024-05-24 07:05:50 -0700

    Add files via upload

---
+++ projectnumber2/serverapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+def timefunction(request):
+	date=datetime.datetime.now()
+	s='<h1>CURRENT TIME IS:'+str(date)+'</h1>'
+	return HttpResponse(s)
